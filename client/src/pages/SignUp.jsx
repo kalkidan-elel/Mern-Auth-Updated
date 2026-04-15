02a87e51f2bae9b82faf32a6f1be7ff0135b7170
Kalkidan <kal4diffacc@gmail.com>
2026-04-15 00:47:03 -0700

    Complete the sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
      const [formData, setFormData] = useState({});
      const [error, setError] = useState(false);
      const [loading, setLoading] = useState(false);
+     const navigate = useNavigate();
      const handleChange = (e) => {
           setFormData({ ...formData, [e.target.id]: e.target.value });
      };
@@ -28,6 +29,7 @@ export default function SignUp() {
                     setError(true);
                     return;
                }
+               navigate('/signin');
           } catch (error) {
                setLoading(false);
                setError(true);
@@ -68,7 +70,7 @@ export default function SignUp() {
                </form>
                <div className='flex gap-2 mt-5'>
                     <p>Have an account?</p>
-                    <Link to='/sign-in'>
+                    <Link to='/signin'>
                          <span className='text-blue-500'>Sign in</span>
                     </Link>
 
